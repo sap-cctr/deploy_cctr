b9f072d3a957d5e229f5af59432b3f25489fe35d
sap-cctr <zdenko@freemail.hu>
2015-11-04 10:51:03 +0000

    Added cookbook
    Added deploy cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'deploy_cctr'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures deploy_cctr'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/copy_install_media.rb
@@ -0,0 +1,5 @@
+home_dir="C:\\SAP\\ContactCenter\\"
+
+directory "#{home_dir}insatll_media" do
+	recursive true
+end
