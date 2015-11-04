c9545875cdba5619250f2f0cabe89eb166146aae
sap-cctr <zdenko@freemail.hu>
2015-11-04 13:34:53 +0000

    Added key reading

--- recipes/copy_install_media.rb
+++ recipes/copy_install_media.rb
@@ -1,5 +1,13 @@
+Chef::Log.info "---------------- #{node['hostname']} -------------------"
+log "Attribute NodeName #{node['NodeName']}"
+Chef::Log.info "Chef::#{cookbook_name}::#{recipe_name}"
+
 home_dir="C:\\SAP\\ContactCenter\\"
 
 directory "#{home_dir}insatll_media" do
 	recursive true
+end
+
+log "Chef run uses key #{node[:aws_access_key_id]}" do
+  level:info
 end
